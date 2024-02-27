f0c18a396f6e8c2f80e5328652f1659d89d3dd28
gabuzdova <zuzana.gabuzdova@gmail.com>
2024-02-27 22:10:59 +0000

    Defines functions to download datasets using credentials.

--- db_utils.py
+++ db_utils.py
@@ -5,13 +5,14 @@ import pandas as pd
 def loadcredentials():
     import yaml
     from yaml.loader import SafeLoader
-    with open(r"/home/zuzana/Desktop/AICORE kurz/AICORE/EXPLORATORY DATA ANALYSIS CUSTOMER LOANS IN FINANCE/credentials.yaml") as credentialsfile:
+    with open(r"/home/zuzana/Desktop/AICORE/AICORE/EXPLORATORY DATA ANALYSIS CUSTOMER LOANS IN FINANCE/credentials.yaml") as credentialsfile:
         data = yaml.load(credentialsfile, Loader=SafeLoader)
         return(data)
 def initialisesqlalchemy():
     import psycopg2
     import sqlalchemy
     from sqlalchemy import create_engine
+    import pandas as pd
 
     hst=(list(loadcredentials().values())[0])
     pswd=(list(loadcredentials().values())[1])
@@ -26,4 +27,17 @@ def initialisesqlalchemy():
     DATABASE=dtbs
     ENDPOINT=hst
     engine= create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-initialisesqlalchemy()
+    return engine
+def extract_data_as_dataframe():
+    sql_data= pd.read_sql_table('loan_payments', initialisesqlalchemy())
+    df=pd.DataFrame(sql_data)
+    return df
+def save_table_local():
+    sql_query= pd.read_sql_table('loan_payments', (initialisesqlalchemy()))
+    df=pd.DataFrame(sql_query)
+    df.to_csv("local.csv")
+def load_data_from_local_machine():
+    import pandas as pd
+    local_database = pd.read_csv(r"/home/zuzana/Desktop/AICORE/AICORE/EXPLORATORY DATA ANALYSIS CUSTOMER LOANS IN FINANCE/local.csv")
+    df = pd.DataFrame(local_database)
+    return(df)

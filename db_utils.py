a37a701577713850ef1ef50693a835213b834658
gabuzdova <zuzana.gabuzdova@gmail.com>
2024-06-07 13:09:17 +0100

    rdsb connector class

--- db_utils.py
+++ db_utils.py
@@ -4,41 +4,34 @@ from sqlalchemy import create_engine
 import psycopg2
 import numpy as np
 import pandas as pd
-import seaborn as sns 
-import matplotlib.pyplot as plt
-import plotly.express as px
 from datetime import datetime
 from dateutil.parser  import parse 
 import yaml
 from yaml.loader import SafeLoader
+import sys
 
 
 #Creating class that connects database I will be working on:
 class RDSDatabaseConnector:
-    def __init__(self, load_credentials):
-        self.loadcredentials = self.load_credentials()
+    def __init__(self, credentials):
+        self.credentials = self.load_credentials(credentials)
 #Function that loads credentials:    
-    def load_credentials(self):
-        with open(self.loadcredentials) as credentialsfile:
+    def load_credentials(self, credentials):
+        with open(credentials, "r") as credentialsfile:
             data = yaml.load(credentialsfile, Loader=SafeLoader)
         return(data)
                 
     
     def initialisesqlalchemy(self):
-                hst=(list(self.load_credentials().values())[0])
-                pswd=(list(self.load_credentials().values())[1])
-                usr=(list(self.load_credentials().values())[2])
-                dtbs=(list(self.load_credentials().values())[3])
-                prt=(list(self.load_credentials().values())[4])
+                ENDPOINT = self.credentials["RDS_HOST"]
+                PASSWORD = self.credentials["RDS_PASSWORD"]
+                USER = self.credentials['RDS_USER']
+                DATABASE = self.credentials['RDS_DATABASE']
+                PORT= self.credentials['RDS_PORT']
                 DATABASE_TYPE= "postgresql"
                 DBAPI="psycopg2"
-                USER=usr
-                PASSWORD=pswd
-                PORT=prt
-                DATABASE=dtbs
-                ENDPOINT=hst
                 engine= create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-                return engine
+                return engine.connect()
     
     def extract_data_as_dataframe(self):
             sql_data= pd.read_sql_table('loan_payments', self.initialisesqlalchemy())
@@ -57,4 +50,14 @@ class RDSDatabaseConnector:
 
 #Class that converts columns to right datatype:
 if __name__ == "__main__":
-       e = RDSDatabaseConnector()
+    #sys.path.append("/home/zuzana/Desktop/exploratory-data-analysis---customer-loans-in-finance282/credentials.yaml")
+    db_connector = RDSDatabaseConnector("credentials.yaml")
+    #print(db_connector.credentials)
+    engine = db_connector.initialisesqlalchemy()
+    #print(engine)
+    df = (db_connector.extract_data_as_dataframe())
+    #print(df)
+    svtable = db_connector.save_table_local()
+    #print(svtable)
+    ldcred = db_connector.load_data_from_local_machine()
+    print(ldcred)
